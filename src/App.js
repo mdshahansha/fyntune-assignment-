95307e91b9b2043a78a45fb8430b8135d6d257ed
Mohammad Shahanhsa <mdshahansha44411@gmail.com>
2021-08-12 00:10:46 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="grid-container">
+               <header>
+                 <a href="/">React </a>
+               </header>
+               <main>Shop Open</main>
+               <footer>All right is reversed</footer>
+    </div>
+  );
+}
+
+export default App;
